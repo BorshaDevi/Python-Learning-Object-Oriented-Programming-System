39f4e0192196d1b87af18f91aab5da35f39c10b0
Borsha Devi <borshadevi27@gmail.com>
2026-01-15 10:47:56 +0600

    Now I learned about class and object

---
+++ OOP Part 1/Class and Object.py
@@ -0,0 +1,9 @@
+# Class is a blueprint for creating one or more objects.
+
+# Creating class
+class Student:   # Student is a class name. By convention, class names start with a capital letter.
+    name='Anu'
+
+# Creating object(instance)  instance and object both are same .
+s1=Student()
+print(s1.name) #class variable access 
