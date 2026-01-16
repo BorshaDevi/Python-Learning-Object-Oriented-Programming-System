dc64d76ed4ea6872db899b8fbdbb809228bf6454
Borsha Devi <borshadevi27@gmail.com>
2026-01-16 10:04:46 +0600

    Now I learned about constructor in little bit and know that __init__ method is a constructor

---
+++ OOP Part 1/Constructor.py
@@ -0,0 +1,8 @@
+# A constructor is a special method in Python that is automatically called when an object is created.
+
+# Why use a constructor?
+# Ans: We use a constructor to set initial values when an object is created.
+
+# __init__  function
+# This is a Python special method which is  called constructor.
+# All classes have a function called __init__() which is always executed when the object is being initiated.
