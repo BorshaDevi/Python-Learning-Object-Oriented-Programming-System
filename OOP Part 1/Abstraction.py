2da585d158e6cf0c46c3cf11760ce0ad58cd2164
Borsha Devi <borshadevi27@gmail.com>
2026-01-27 07:54:59 +0600

    correct accelerator word

--- OOP Part 1/Abstraction.py
+++ OOP Part 1/Abstraction.py
@@ -1,15 +1,15 @@
 # Abstraction means hiding the complex implementation details of a class and showing only the important things. 
 class Car:
     def __init__(self):
-        self.acceletor=False
+        self.accelerator=False
         self.brk=False
         self.clutch=False
     def start(self):
-        self.acceletor=True
+        self.accelerator=True
         self.clutch=True
         print("Car Started...")
 car1=Car()
 car1.start()        
 #  see here I call start() to use the car. 
-# I don't need to know the internal details like how the acceletor and clutch  work.
+# I don't need to know the internal details like how the accelerator and clutch  work.
 # This is abstraction — hiding complexity and showing only the important functionality.
