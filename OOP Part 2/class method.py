8dabf217252ad5303d03a5e022687aa4e56b512e
Borsha Devi <borshadevi27@gmail.com>
2026-02-04 10:05:30 +0600

    Now I learned about @classmethod properly which is use for change the class attribute

--- OOP Part 2/class method.py
+++ OOP Part 2/class method.py
@@ -1,2 +1,26 @@
-# A class method is bound to the class & receives the class as an implicit first argument.
+# A class method is a method that is related to the class and does not require any specific object.
 # Note- static method can't access or modify class state & generally for utility.
+
+class Person:
+    name='anonymous'
+    def hello(self,name):
+        # print('hello',Person.name)  
+        # see here,I access class attribute with Person.name.then question is ,how can I change the class attribute?
+        #  there are so many way, 
+        Person.name=name
+        print('1st way to change the class attribute',name)
+        self.__class__.name=name
+        print('2nd way to change the class attribute',name)
+p1=Person()
+p1.hello('Alex')  
+print(Person.name)   
+
+# But the best practice is to use @classmethod to change the class attribute.so we use @classmethod.
+class Person:
+    name="anonymous"
+    @classmethod
+    def change_name(cls,name):
+        cls.name=name
+p1=Person()
+p1.change_name('Alex')
+print(Person.name)
