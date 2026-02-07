79439bbf719faeb1f27d318da8fc3f979bf2012f
Borsha Devi <borshadevi27@gmail.com>
2026-02-07 11:15:10 +0600

    Now I learned about some polymorphism which is same operator or function or method performs in different action depend on data type.

---
+++ OOP Part 2/Polymorphism.py
@@ -0,0 +1,5 @@
+# When the same operator is allowed to have different meanings  according to the context.
+print(1+2) # 3  additions .(number type)
+print("Toma"+"to" ) # Tomato concatenation.(str type)
+print([1,2,3] + [4,5,6]) # [1,2,3,4,5,6] combines two lists. (list type)
+# This is an example of polymorphism  because the same operator performs different actions depending on the data type.
