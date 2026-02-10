392698bd8373cfaf5368f3fcf8ebbf6bb9614c93
Borsha Devi <borshadevi27@gmail.com>
2026-02-10 21:43:15 +0600

    learned some polymorphism

--- OOP Part 2/Polymorphism.py
+++ OOP Part 2/Polymorphism.py
@@ -4,3 +4,12 @@ print(1+2) # 3  additions .(number type)
 print("Toma"+"to" ) # Tomato concatenation.(str type)
 print([1,2,3] + [4,5,6]) # [1,2,3,4,5,6] combines two lists. (list type)
 # This is an example of polymorphism  because the same operator performs different actions depending on the data type.
+
+class Complex:
+    def __init__(self,real,img):
+        self.real=real
+        self.img=img
+    def showNumber(self):
+        print(self.real,"i +",self.img,"j")    
+num1=Complex(1,4)
+num1.showNumber()        
