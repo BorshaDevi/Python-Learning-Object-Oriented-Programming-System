3ae0d03a54611f9eac6eee535debeed7ef2c97b2
Borsha Devi <borshadevi27@gmail.com>
2026-02-25 22:03:50 +0600

    now i practice q:no 29 in Practice.py file

--- Practice.py
+++ Practice.py
@@ -622,34 +622,42 @@
 # multi=(v1 * v2)
 # print(multi)
 
-# 28: Employee Sorting
-class Employee:
-    def __init__(self,name,salary):
-        self.name=name
-        self.salary=salary
-    def __lt__(self,other):
-        return self.salary <= other.salary
-    def __eq__(self,other):
-        return self.salary== other.salary
-    def __repr__(self):
-        return (f'{self.name} and {self.salary}')
-employee=[
-    Employee('Helen',50000),
-    Employee('Alex',20000),
-    Employee('Eva',30000),
+# # 28: Employee Sorting
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.salary=salary
+#     def __lt__(self,other):
+#         return self.salary <= other.salary
+#     def __eq__(self,other):
+#         return self.salary== other.salary
+#     def __repr__(self):
+#         return (f'{self.name} and {self.salary}')
+# employee=[
+#     Employee('Helen',50000),
+#     Employee('Alex',20000),
+#     Employee('Eva',30000),
     
-    ]
-employee.sort()
-print(employee)
+#     ]
+# employee.sort()
+# print(employee)
 
 
-# # 29: Custom Range Class
-# class Range():
-#     def __init__(self,start,stop):
-#         self.start=start
-#         self.stop=stop
-#     def __iter__(self):
-#         pass
-#     def __next__(self):
-#         pass
-# r1=Range()    
+# 29: Custom Range Class
+class Range():
+    def __init__(self,start,stop):
+        self.start=start
+        self.stop=stop
+    def __iter__(self):
+       self.current=self.start
+       return self
+    def __next__(self):
+        if(self.current>=self.stop):
+            raise StopIteration  
+        else:
+           value=self.current
+           self.current +=1
+           return value
+range1=Range(1,5)    
+for i in range1:
+    print(i)
